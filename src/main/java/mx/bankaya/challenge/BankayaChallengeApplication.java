97e8fc9e05acb8632117d00720064511e8732817
jair-ala <jairsauceda@Jairs-MacBook-Pro.local>
2024-11-14 10:57:33 -0600

    init repo

---
+++ src/main/java/mx/bankaya/challenge/BankayaChallengeApplication.java
@@ -0,0 +1,13 @@
+package mx.bankaya.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankayaChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankayaChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/bankaya/challenge/BankayaChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.bankaya.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankayaChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
